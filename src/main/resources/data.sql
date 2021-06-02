f12b6893e7e552071b2c0b78d92730f4815ef8d9
canyavuzkurt <can_yavuzkurt@hotmail.com>
2021-06-02 19:45:03 +0300

    new data for sql 2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into users (created_at, f_name, l_name, api_token) values (current_time , 'Caner', 'Tanguler', '109588342479223870141');
+insert into users (created_at, f_name, l_name, api_token) values (current_time , 'Caner', 'Tanguler', '114227698111863797827');
 
 insert into recommendations (created_at, neighbourhood, price, property_type, user_id) values (current_time , 'Boston House 1', 127, 'Entire apartment', 1);
 insert into recommendations (created_at, neighbourhood, price, property_type, user_id) values (current_time , 'NY Hotel Room 1', 108, 'Room in hotel', 1);
